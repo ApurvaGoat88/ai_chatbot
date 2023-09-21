dfc10ed996840122852cd3149c7142bb785559e3
APurVA <131962594+ApurvaGoat88@users.noreply.github.com>
2023-09-21 23:30:16 +0530

    secret.dart

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,2 +1,2 @@
-const OPENAIKEY = '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';
-const chatkey = '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';
+const OPENAIKEY = '$key$';
+const chatkey = '$key$';
